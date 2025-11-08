49f766e3f502f387637e46616e32cff9e18c0f6b
thien do <spiritaway8055@gmail.com>
2025-11-08 22:28:39 +0700

    [MGE-1] Config port for service

--- Microservices/Inventory/Inventory.API/Program.cs
+++ Microservices/Inventory/Inventory.API/Program.cs
@@ -1,9 +1,16 @@
+using Common.RestApi;
+using Common.RestApi.Extension;
+using Inventory.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.UpdateRuntimeContext(builder.Configuration);
+builder.Services.AddDbContext<DbContext, InventoryDbContext>(options => options.UseNpgsql("Server=localhost;Database=demo;User Id=postgres;Password=1qaz2wsxE"));
+builder.Services.AddMessageBus();
+builder.WebHost.ConfigDynamicKestrel(builder.Configuration, RuntimeContext.CommonConfig.Microservices.InventoryPort);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
