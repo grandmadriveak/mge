d026b7f5ed357c36ae6d61cc0246dae48764c212
thien do <spiritaway8055@gmail.com>
2025-11-09 20:56:35 +0700

    [MGE-1] Fixing const

--- Microservices/Shipping/Shipping.Services/EventHandler/DemoEventHandler.cs
+++ Microservices/Shipping/Shipping.Services/EventHandler/DemoEventHandler.cs
@@ -1,10 +1,5 @@
 ﻿using Common.RestApi.Constants;
 using DotNetCore.CAP;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shipping.Services.EventHandler
 {
